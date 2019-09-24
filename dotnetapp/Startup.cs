3924fc2c55541889ab498889d9e8c498211f94af
Giancarlos Salas <giansalex@gmail.com>
2019-09-24 12:17:53 -0500

    add health checks

--- dotnetapp/Startup.cs
+++ dotnetapp/Startup.cs
@@ -18,6 +18,7 @@ namespace dotnetapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks();
             services.AddControllersWithViews();
         }
 
@@ -38,6 +39,7 @@ namespace dotnetapp
             
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHealthChecks("/health");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}"
